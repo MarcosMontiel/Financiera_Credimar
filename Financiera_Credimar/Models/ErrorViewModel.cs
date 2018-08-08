1928c6af0e3e242b16fdd542ef9da5aebf2d0c66
Marcos Montiel <marcos-gab14@hotmail.com>
2018-08-07 23:30:12 -0500

    Inserción inicial en el repositorio del módulo Financiera_Credimar

---
+++ Financiera_Credimar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Financiera_Credimar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
