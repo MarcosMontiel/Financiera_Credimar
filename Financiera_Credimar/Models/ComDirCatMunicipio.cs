89d03692e493ed269bf92c309437f475c9a5b89a
Marcos Montiel <marcos-gab14@hotmail.com>
2018-08-08 00:00:30 -0500

    Entidades y DataContext agregados

---
+++ Financiera_Credimar/Models/ComDirCatMunicipio.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Financiera_Credimar.Models
+{
+    public class ComDirCatMunicipio
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Valor { get; set; }
+
+        public int FKComDirCatEstado { get; set; }
+        [ForeignKey("FKComDirCatEstado")]
+        public ComDirCatEstado ComDirCatEstado { get; set; }
+    }
+}
---
+++ Financiera_Credimar/Models/UsuUsuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Financiera_Credimar.Models
+{
+    public class UsuUsuario
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string User { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        public int FKEmpEmpleado { get; set; }
+        [ForeignKey("FKEmpEmpleado")]
+        public EmpEmpleado EmpEmpleado { get; set; }
+
+        public int FKUsuCatRol { get; set; }
+        [ForeignKey("FKUsuCatRol")]
+        public UsuCatRol UsuCatRol { get; set; }
+
+        public int FKUsuCatEstado { get; set; }
+        [ForeignKey("FKUsuCatEstado")]
+        public UsuCatEstado UsuCatEstado { get; set; }
+    }
+}
